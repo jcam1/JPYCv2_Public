8cc8815bada095fa267902c90575ba81a42676a4
satosato23 <17274001+satosato23@users.noreply.github.com>
2024-09-17 14:53:54 +0900

    test token発行用ファイル追加

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,38 +1,44 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-
-require('@nomiclabs/hardhat-truffle5')
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-viem";
+import '@nomiclabs/hardhat-truffle5';
 // require("@nomiclabs/hardhat-waffle")
-require('@openzeppelin/hardhat-upgrades')
-require('hardhat-contract-sizer')
-require('solidity-coverage')
-require('dotenv').config()
-
-module.exports = {
-  solidity: {
-    compilers: [{
-      version: '0.8.11',
-      settings: {
-        optimizer: {
-          enabled: true,
-          runs: 3000,
+import '@openzeppelin/hardhat-upgrades';
+import 'hardhat-contract-sizer';
+import 'solidity-coverage';
+import 'dotenv';
+ 
+const config: HardhatUserConfig = {
+    solidity: {
+      compilers: [
+        {
+          version: '0.8.11',
+          settings: {
+            optimizer: {
+              enabled: true,
+              runs: 3000,
+            },
+          },
+        },
+        {
+          version: '0.4.24',
+        }
+      ]
+    },
+  
+    networks: {
+      hardhat: {
+        chainId: 1337,
+        allowUnlimitedContractSize: false,
+      },
+      
+      anvil: {
+        url: "http://127.0.0.1:8545",
+        chainId: 31337,
+        accounts: {
+          mnemonic: "test test test test test test test test test test test junk",
         },
       },
     },
-    {
-      version: '0.4.24',
-    }
-  ]},
-
-  networks: {
-    hardhat: {
-      chainId: 1337,
-      allowUnlimitedContractSize: false,
-    },
-    rinkeby: {
-      url: process.env.INFRA_API_KEY,
-      accounts: [process.env.PRIVATE_KEY],
-    },
-  },
-}
+  };
+  
+  export default config;
